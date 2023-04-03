2b645adc2b9b37758060dbe6083bfbb22b3054da
Larysa Semerikova <lorik280319@gmail.com>
2023-04-03 16:51:13 +0300

    fixed some moments

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -2,13 +2,20 @@
 
 const menuBtn = document.querySelector('.burger-menu')
 const menu = document.querySelector('.header-menu__items')
+const body = document.querySelector('body')
 
 document.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.closest('.burger-menu') ||
-        !e.target.closest('.header-menu__items')
-    ) {
+        !e.target.closest('.header-menu__items') &&
+        body.classList.contains('lock')
+        )
+    {
         menuBtn.classList.toggle('active')
         menu.classList.toggle('active')
+        body.classList.toggle('lock')
     }
 })
+
+
+
